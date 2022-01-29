0e3c46111fe00c1c953c9b69c360f41c9702549c
Praveen Kumar.M <praveenmkumar893@gmail.com>
2022-01-29 18:05:49 +0530

    WorK

--- src/main/java/com/airlinereservationsystemapp/Controller/CancelTicketServlet.java
+++ src/main/java/com/airlinereservationsystemapp/Controller/CancelTicketServlet.java
@@ -12,7 +12,6 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
-import javax.websocket.Session;
 
 import com.airlinereservationsystemapp.DaoImpl.Passenger_detailsDao;
 import com.airlinereservationsystemapp.Models.Passenger_details;
